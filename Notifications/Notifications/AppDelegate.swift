476e802f92f8a6f78dc7557fa8c7b0bea6ded48d
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-02-02 15:47:45 +0700

    Added action notifications

--- Notifications/Notifications/AppDelegate.swift
+++ Notifications/Notifications/AppDelegate.swift
@@ -36,30 +36,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print("Notification settings: \(settings)")
         }
     }
+    
+    // метод отвечает за расписание уведомлений
+    func scheduleNotification(notificationType: String) {
         
-        // метод отвечает за расписание уведомлений
-        func scheduleNotification(notificationType: String) {
-            
-            let content = UNMutableNotificationContent()
-            
-            content.title = notificationType
-            content.body = "This is example how to create" + notificationType
-            content.sound = UNNotificationSound.default
-            content.badge = 1
-            
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-            let identifiere = "Local Notification"
-            let request = UNNotificationRequest(identifier: identifiere,
-                                                content: content,
-                                                trigger: trigger)
-            
-            notificationCenter.add(request) { (error) in
-                if let error = error {
-                    print("Error \(error.localizedDescription)")
-                }
+        let content = UNMutableNotificationContent()
+        let userAction = "User Action"
+        
+        content.title = notificationType
+        content.body = "This is example how to create" + notificationType
+        content.sound = UNNotificationSound.default
+        content.badge = 1
+        content.categoryIdentifier = userAction
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let identifiere = "Local Notification"
+        let request = UNNotificationRequest(identifier: identifiere,
+                                            content: content,
+                                            trigger: trigger)
+        
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
             }
         }
+        // создание действий пользователей в уведомлении (можно отображать до 4 действий)
+        let snoozeAction = UNNotificationAction(identifier: "Snooze",
+                                                title: "Snooze",
+                                                options: [])
+        
+        let deleteAction = UNNotificationAction(identifier: "Delete",
+                                                title: "Delete",
+                                                options: [.destructive])
+        
+        let category = UNNotificationCategory(identifier: userAction,
+                                              actions: [snoozeAction, deleteAction],
+                                              intentIdentifiers: [],
+                                              options: [])
+        
+        notificationCenter.setNotificationCategories([category])
     }
+}
 // получать уведомления,когда приложение находится в переднем плане
 extension AppDelegate: UNUserNotificationCenterDelegate {
     
@@ -67,21 +84,33 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         _ center: UNUserNotificationCenter,
         willPresent notification: UNNotification,
         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
-        completionHandler([.alert, .sound])
-    }
+            
+            completionHandler([.alert, .sound])
+        }
     
     func userNotificationCenter(
         _ center: UNUserNotificationCenter,
         didReceive response: UNNotificationResponse,
         withCompletionHandler completionHandler: @escaping () -> Void) {
-        
-        if response.notification.request.identifier == "Local Notification" {
-            print("Handling notification with the Local Notification Identifire")
+            
+            if response.notification.request.identifier == "Local Notification" {
+                print("Handling notification with the Local Notification Identifire")
+            }
+            
+            switch response.actionIdentifier {
+                case UNNotificationDismissActionIdentifier:
+                    print("Dismiss Action")
+                case UNNotificationDefaultActionIdentifier:
+                    print("Default")
+                case "Snooze":
+                    scheduleNotification(notificationType: "Reminder")
+                case "Delete":
+                    print("Delete")
+                default:
+                    print("Unknown action")
+            }
+            completionHandler()
         }
-        
-        completionHandler()
-    }
 }
 
 
